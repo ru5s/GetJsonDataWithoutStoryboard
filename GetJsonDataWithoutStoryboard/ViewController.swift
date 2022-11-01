fd49483ac6bede62b81106c10209603af927869c
ru5s <hellperock@gmail.com>
2022-11-01 22:12:51 +0500

    Initial Commit

---
+++ GetJsonDataWithoutStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetJsonDataWithoutStoryboard
+//
+//  Created by Ruslan Ismailov on 01/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
